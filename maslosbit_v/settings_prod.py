909e31c20e6fe2db73fc9b480b142ef885ffcdb3
EchoFoe <Aspirpd@gmail.com>
2018-06-21 13:14:53 +0300

    com_2

---
+++ maslosbit_v/settings_prod.py
@@ -0,0 +1,16 @@
+DEBUG = False
+ALLOWED_HOSTS = ['207.154.255.208',
+                 '.maslosbit.ru']
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db1',
+        'USER': 'maslosbit',
+        'PASSWORD': 'echofoe103',
+        'HOST': 'localhost',
+        'PORT': '',
+
+    }
+}
